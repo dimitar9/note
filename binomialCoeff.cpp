cf793d5fa6ea4628aa88179ddd54ad9159e45f45
Dimitar9 <dimitar9909@gmail.com>
2014-07-28 10:51:25 -0400

    Create binomialCoeff.cpp

---
+++ binomialCoeff.cpp
@@ -0,0 +1,24 @@
+// Returns value of Binomial Coefficient C(n, k)
+int binomialCoeff(int n, int k)
+{
+    int C[n+1][k+1];
+    int i, j;
+ 
+    // Caculate value of Binomial Coefficient in bottom up manner
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= min(i, k); j++)
+        {
+            // Base Cases
+            if (j == 0 || j == i)
+                C[i][j] = 1;
+ 
+            // Calculate value using previosly stored values
+            else
+                C[i][j] = C[i-1][j-1] + C[i-1][j];
+        }
+    }
+ 
+    return C[n][k];
+}
+//http://www.csl.mtu.edu/cs4321/www/Lectures/Lecture%2015%20-%20Dynamic%20Programming%20Binomial%20Coefficients.htm 
